1abc54996ea1d1234fa2570ba088401f93deda5b
Kaua lima <120141068+kauadev1@users.noreply.github.com>
2023-01-03 10:52:29 -0300

    commit-cap24-aula11
    Elemento output em formulários HTML

---
+++ ex034-Formularios/form008.js
@@ -0,0 +1,7 @@
+
+function calcIdade() {
+  let atual = new Date().getFullYear()
+  iidade.innerHTML = Number(atual) - Number(iano.value)
+}
+
+//Para comprender o comando acima, aprender sobre java
